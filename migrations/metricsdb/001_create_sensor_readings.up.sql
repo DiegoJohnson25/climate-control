2cb3dc358095efdf8f715145bebc41b4ff7d3f9e
Diego Johnson <diegoajohnson@gmail.com>
2026-04-19 09:53:25 -0400

    feat: add migration scripts for room control logs and update sensor readings table

--- migrations/metricsdb/001_create_sensor_readings.up.sql
+++ migrations/metricsdb/001_create_sensor_readings.up.sql
@@ -1,12 +1,17 @@
+BEGIN;
+
 CREATE TABLE sensor_readings (
     time      TIMESTAMPTZ NOT NULL,
     sensor_id UUID NOT NULL,
     room_id   UUID,
-    value     NUMERIC NOT NULL
+    value     NUMERIC NOT NULL,
+    raw_value NUMERIC NOT NULL
 );
 
 SELECT create_hypertable('sensor_readings', 'time',
-    chunk_time_interval => INTERVAL '7 days');
+    chunk_time_interval => INTERVAL '1 day');
 
 CREATE INDEX idx_sensor_readings_sensor_time ON sensor_readings(sensor_id, time DESC);
 CREATE INDEX idx_sensor_readings_room_time   ON sensor_readings(room_id, time DESC);
+
+COMMIT;
---
+++ migrations/metricsdb/002_create_room_control_logs.up.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+CREATE TABLE room_control_logs (
+    time               TIMESTAMPTZ NOT NULL,
+    room_id            UUID NOT NULL,
+    avg_temp           NUMERIC,
+    avg_hum            NUMERIC,
+    mode               TEXT CHECK (mode IN ('OFF', 'AUTO')),
+    target_temp        NUMERIC,
+    target_hum         NUMERIC,
+    control_source     TEXT CHECK (control_source IN ('manual_override', 'schedule', 'grace_period', 'none')),
+    heater_cmd         SMALLINT CHECK (heater_cmd IN (0, 1)),
+    humidifier_cmd     SMALLINT CHECK (humidifier_cmd IN (0, 1)),
+    reading_count_temp SMALLINT,
+    reading_count_hum  SMALLINT,
+    schedule_period_id UUID
+);
+
+SELECT create_hypertable('room_control_logs', 'time',
+    chunk_time_interval => INTERVAL '1 day');
+
+CREATE INDEX idx_room_control_logs_room_time ON room_control_logs(room_id, time DESC);
+CREATE INDEX idx_room_control_logs_period    ON room_control_logs(schedule_period_id) WHERE schedule_period_id IS NOT NULL;
+
+COMMIT;
